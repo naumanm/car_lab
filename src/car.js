b7f97810370e2cbd9b216fcade7447a5d18a008e
michael nauman <bodhiandmike@gmail.com>
2015-01-21 20:27:24 -0800

    initial commit

--- src/car.js
+++ src/car.js
@@ -1,13 +1,29 @@
-function Car(make, model, year, color){
-
+function Car(make, model, year, state, color, current_owner, passengers){
+	this.make = make;
+	this.model = model;
+	this.year = year;
+	this.state= off;
+	this.color = color;
+	this.current_owner = "Manufacturer";
+	this.passengers = passengers;
 }
 
-Car.prototype.sale = function(newOwner){
+Car.prototype.previous_owners = [];
 
+Car.prototype.sale = function(newOwner){
+	this.previous_owner.push(newOwner);
+	this.newOwner = newOwner;
 };
 
 Car.prototype.paint = function(newColor){
-
+	this.color = newColor;
 };
+
+Car.prototype.start = this.state(on);
+
+Car.prototype.off = this.state(off);
+
+
+
 
 module.exports=Car;
